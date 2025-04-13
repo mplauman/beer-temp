4e7f4bfc2c673e20efa577d7cf4fb1e1c42616bf
Matt Plaumann <matt@plaumann.ca>
2025-04-13 09:43:53 -0400

    Add WIFI connectivity

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+#include "secrets.hpp"
+
+void connectToWiFi() {
+  Serial.print("Connecting to WiFi");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected to WiFi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
 
 void setup() {
   Serial.begin(115200); // See `monitor_speed` in platformio.ini
   delay(2000);
+
+  connectToWiFi();  
 }
 
 void loop() {
