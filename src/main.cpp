532579dd265b0eec9d76d7cc994088db999b18e4
Matt Plaumann <matt@plaumann.ca>
2025-04-14 20:41:33 -0400

    Add support for writing to statsd

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
 #include "secrets.hpp"
 
@@ -19,12 +20,38 @@ void connectToWiFi() {
 
 void setup() {
   Serial.begin(115200); // See `monitor_speed` in platformio.ini
-  delay(2000);
-
-  connectToWiFi();  
 }
 
 void loop() {
-  Serial.println("Hello, world!");
-  delay(1000);
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("No WiFI connection, reconnecting...");
+    connectToWiFi();
+  }
+
+  do {
+    static char buffer[1024];
+    int written = snprintf(buffer, sizeof(buffer), "%s:%d|g\n", STATSD_BUCKET, 1);
+    if (written <= 0 || written >= sizeof(buffer)) {
+      Serial.println("Error writing to buffer");
+      break;
+    }
+
+    WiFiUDP udp;
+    if (!udp.beginPacket(STATSD_SERVER, STATSD_PORT)) {
+      Serial.println("Connection to statsd server failed");
+      break;
+    }
+    if (!udp.write(buffer, written)) {
+      Serial.println("Failed to write to UDP packet");
+      break;
+    }
+    if (!udp.endPacket()) {
+      Serial.println("Failed to send UDP packet");
+      break;
+    }
+
+    Serial.print(buffer); // includes a newline
+  } while (false);
+
+  delay(5000);
 }
