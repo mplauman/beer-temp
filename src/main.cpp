69152f36323586daaf31abe7be75e15a7fcfd8a6
Matt Plaumann <matt@plaumann.ca>
2025-04-18 19:09:25 -0400

    Add temp control

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,63 @@
+#include <Adafruit_AHTX0.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <Wire.h>
 
 #include "secrets.hpp"
 
-void connectToWiFi() {
-  Serial.print("Connecting to WiFi");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+namespace {
+  Adafruit_AHTX0 aht;
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  void connectToWiFi() {
+    Serial.print("Connecting to WiFi");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+  
+    Serial.println("\nConnected to WiFi");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
   }
+  
+  bool writeFloat(WiFiUDP &udp, const char *const bucket, float value) {
+    char buffer[1024];
 
-  Serial.println("\nConnected to WiFi");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+    int written = snprintf(buffer, sizeof(buffer), "%s:%.2f|g\n", bucket, value);
+    if (written <= 0 || written >= sizeof(buffer)) {
+      Serial.println("Error writing to buffer");
+      return false;
+    }
+  
+    if (!udp.beginPacket(STATSD_SERVER, STATSD_PORT)) {
+      Serial.println("Connection to statsd server failed");
+      return false;
+    }
+
+    if (!udp.write(buffer, written)) {
+      Serial.println("Failed to write to UDP packet");
+      return false;
+    }
+
+    if (!udp.endPacket()) {
+      Serial.println("Failed to send UDP packet");
+      return false;
+    }
+
+    return true;
+  }
 }
 
 void setup() {
   Serial.begin(115200); // See `monitor_speed` in platformio.ini
+  
+  while (!aht.begin()) {
+    Serial.println("Failed to initialize AHT21 sensor!");
+    delay(1000);
+  }
 }
 
 void loop() {
@@ -29,28 +67,22 @@ void loop() {
   }
 
   do {
-    static char buffer[1024];
-    int written = snprintf(buffer, sizeof(buffer), "%s:%d|g\n", STATSD_BUCKET, 1);
-    if (written <= 0 || written >= sizeof(buffer)) {
-      Serial.println("Error writing to buffer");
+    static sensors_event_t humidity, temp;
+    if (!aht.getEvent(&humidity, &temp)) {
+      Serial.println("Failed to read from AHT21 sensor");
       break;
     }
 
     WiFiUDP udp;
-    if (!udp.beginPacket(STATSD_SERVER, STATSD_PORT)) {
-      Serial.println("Connection to statsd server failed");
-      break;
-    }
-    if (!udp.write(buffer, written)) {
-      Serial.println("Failed to write to UDP packet");
-      break;
-    }
-    if (!udp.endPacket()) {
-      Serial.println("Failed to send UDP packet");
-      break;
+    if (!writeFloat(udp, STATSD_TEMPERATURE_BUCKET, temp.temperature)) {
+      Serial.println("Failed to record temperature");
     }
 
-    Serial.print(buffer); // includes a newline
+    if (!writeFloat(udp, STATSD_HUMIDITY_BUCKET, humidity.relative_humidity)) {
+      Serial.println("Failed to record humidity");
+    }
+
+    Serial.printf("temperature: %.2f °C, humidity: %.2f%%\n", temp.temperature, humidity.relative_humidity);
   } while (false);
 
   delay(5000);
